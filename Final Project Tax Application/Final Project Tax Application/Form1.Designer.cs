bb81e4703942f8799394e6655715596a71a1404d
tysmith19 <76920650+tysmith19@users.noreply.github.com>
2021-01-03 13:05:55 -0800

    Add files via upload

---
+++ Final Project Tax Application/Final Project Tax Application/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Final_Project_Tax_Application
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblHourlyRate = new System.Windows.Forms.Label();
+            this.lblHoursWorked = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtHourlyRate = new System.Windows.Forms.TextBox();
+            this.txtHoursWorked = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.lstEmployees = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(12, 9);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(60, 13);
+            this.lblFirstName.TabIndex = 0;
+            this.lblFirstName.Text = "First Name:";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(80, 9);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(61, 13);
+            this.lblLastName.TabIndex = 1;
+            this.lblLastName.Text = "Last Name:";
+            // 
+            // lblHourlyRate
+            // 
+            this.lblHourlyRate.AutoSize = true;
+            this.lblHourlyRate.Location = new System.Drawing.Point(12, 55);
+            this.lblHourlyRate.Name = "lblHourlyRate";
+            this.lblHourlyRate.Size = new System.Drawing.Size(39, 13);
+            this.lblHourlyRate.TabIndex = 2;
+            this.lblHourlyRate.Text = "Wage:";
+            // 
+            // lblHoursWorked
+            // 
+            this.lblHoursWorked.AutoSize = true;
+            this.lblHoursWorked.Location = new System.Drawing.Point(80, 55);
+            this.lblHoursWorked.Name = "lblHoursWorked";
+            this.lblHoursWorked.Size = new System.Drawing.Size(79, 13);
+            this.lblHoursWorked.TabIndex = 3;
+            this.lblHoursWorked.Text = "Hours Worked:";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(15, 26);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(65, 20);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(83, 26);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(65, 20);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // txtHourlyRate
+            // 
+            this.txtHourlyRate.Location = new System.Drawing.Point(15, 71);
+            this.txtHourlyRate.Name = "txtHourlyRate";
+            this.txtHourlyRate.Size = new System.Drawing.Size(65, 20);
+            this.txtHourlyRate.TabIndex = 2;
+            // 
+            // txtHoursWorked
+            // 
+            this.txtHoursWorked.Location = new System.Drawing.Point(83, 71);
+            this.txtHoursWorked.Name = "txtHoursWorked";
+            this.txtHoursWorked.Size = new System.Drawing.Size(65, 20);
+            this.txtHoursWorked.TabIndex = 3;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(162, 102);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(57, 23);
+            this.btnSubmit.TabIndex = 11;
+            this.btnSubmit.Text = "&Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(162, 21);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(54, 70);
+            this.btnEnter.TabIndex = 4;
+            this.btnEnter.Text = "&Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(162, 131);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(57, 23);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(225, 12);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.ReadOnly = true;
+            this.txtDisplay.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDisplay.Size = new System.Drawing.Size(247, 198);
+            this.txtDisplay.TabIndex = 16;
+            this.txtDisplay.TabStop = false;
+            // 
+            // lstEmployees
+            // 
+            this.lstEmployees.FormattingEnabled = true;
+            this.lstEmployees.Location = new System.Drawing.Point(15, 97);
+            this.lstEmployees.Name = "lstEmployees";
+            this.lstEmployees.Size = new System.Drawing.Size(133, 108);
+            this.lstEmployees.TabIndex = 17;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(484, 222);
+            this.Controls.Add(this.lstEmployees);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtHourlyRate);
+            this.Controls.Add(this.txtHoursWorked);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblHoursWorked);
+            this.Controls.Add(this.lblHourlyRate);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Name = "frmMain";
+            this.Text = "Accounting Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblHourlyRate;
+        private System.Windows.Forms.Label lblHoursWorked;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtHourlyRate;
+        private System.Windows.Forms.TextBox txtHoursWorked;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.ListBox lstEmployees;
+    }
+}
+
---
+++ Final Project Tax Application/Final Project Tax Application/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Project_Tax_Application
+{
+    public partial class frmMain : Form
+    {
+
+        Employee[] employees = new Employee[20];
+        int count = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        //Click event handles printing out Employee data onto txtDisplay
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+            txtDisplay.Text = "";
+
+            for (int i = 0; i < count; i++)
+            {
+                txtDisplay.Text += "Name: " + employees[i].FirstName + ", " + employees[i].LastName + "\r\n\tGross Pay: " + (employees[i].grossPay.ToString("C2")) + "\r\n\tNet Pay: " + (employees[i].netPay().ToString("C2")) +
+                "\r\n\tSS Withheld: " + employees[i].SSWithheld().ToString("C2") + "\r\n\tMedicare Withheld: " + employees[i].MedicareWithheld().ToString("C2") + "\r\n\tSIT Withheld: " + employees[i].StateInTax().ToString("C2") + "\r\n\tFIT Withheld: " + employees[i].FedInTax().ToString("C2") + "\r\n";
+            }
+
+        }
+
+        //Click event handles processing data entered and turning it into a Employee object
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+
+            double test;
+            int test1;
+            //Checks if employee array count has exceeded 20
+            if (count <= 19)
+            {
+                //if statement for making sure correct data is entered
+                if (double.TryParse(txtHourlyRate.Text, out test) && int.TryParse(txtHoursWorked.Text, out test1) && !(txtFirstName.Text.Equals("")) && !(txtLastName.Text.Equals("")))
+                {
+
+                    lstEmployees.Items.Add(txtFirstName.Text + ", " + txtLastName.Text);
+                    employees[count] = new Employee(txtFirstName.Text, txtLastName.Text, double.Parse(txtHourlyRate.Text), int.Parse(txtHoursWorked.Text));
+                    ++count;
+
+                    txtFirstName.Text = "";
+                    txtHourlyRate.Text = "";
+                    txtHoursWorked.Text = "";
+                    txtLastName.Text = "";
+
+                }
+                else
+                {
+                    //Outputs message depending on what data was entered incorrectly
+                    if (!(double.TryParse(txtHourlyRate.Text, out test)))
+                    {
+                        MessageBox.Show("Hourly Rate field only excepts number or decimal values");
+                    }
+                    if (!(int.TryParse(txtHourlyRate.Text, out test1)))
+                    {
+                        MessageBox.Show("Hours Worked field only excepts whole number values");
+                    }
+                    if (txtFirstName.Text.Equals(""))
+                    {
+                        MessageBox.Show("Please enter Employee's first name");
+                    }
+                    if (txtLastName.Text.Equals(""))
+                    {
+                        MessageBox.Show("Please enter Employee's last name");
+                    }
+
+                }
+
+            }
+            else
+            {
+                //Message for indicating that the Employee array is full
+                MessageBox.Show("Maximum number of employees entered");
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+
+    public class Employee
+    {
+
+        //initializing class variables
+        private string firstName;
+        private string lastName;
+        private double hourlyRate;
+        private int hoursWorked;
+        public double grossPay;
+
+        //constructor for creating employee object
+        public Employee(string firstName, string lastName, double hourlyRate, int hoursWorked)
+        {
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.hourlyRate = hourlyRate;
+            this.hoursWorked = hoursWorked;
+            grossPay = hoursWorked * hourlyRate;
+
+        }
+
+        //outputs the Net Pay employee has earned
+        public double netPay()
+        {
+            return grossPay - this.SSWithheld() - this.MedicareWithheld() - this.StateInTax() - this.FedInTax();
+        }
+
+        //outputs the Sociali Security tax withheld
+        public double SSWithheld()
+        {
+            return grossPay * .062;
+        }
+
+        //outputs Medicare tax withheld
+        public double MedicareWithheld()
+        {
+            return grossPay * .0145;
+        }
+
+        //Method for returning state income tax for employee depending on amount earned
+        public double StateInTax()
+        {
+
+            if (0 < grossPay && grossPay <= 500)
+            {
+                return (grossPay * .02);
+            }
+            else if (500 <= grossPay && grossPay <= 999.99)
+            {
+                return grossPay * .04;
+            }
+            else if (1000 <= grossPay)
+            {
+                return grossPay * .06;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        //returns federal income tax using a series of if statements selecting the correct percentage to calculate federal income tax 
+        public double FedInTax()
+        {
+
+            if (0 < grossPay && grossPay < 500)
+            {
+                return (grossPay * .05);
+            }
+            else if (500 <= grossPay && grossPay <= 999.99)
+            {
+                return grossPay * .10;
+            }
+            else if (1000 <= grossPay && grossPay <= 1499.99)
+            {
+                return grossPay * .15;
+            }
+            else if (1500 <= grossPay && grossPay <= 1999.99)
+            {
+                return (grossPay * .20);
+            }
+            else if (2000 <= grossPay && grossPay <= 2999.99)
+            {
+                return (grossPay * .25);
+            }
+            else if (3000 <= grossPay)
+            {
+                return (grossPay * .30);
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public string FirstName
+        {
+
+            get
+            {
+                return firstName;
+            }
+
+        }
+
+        public string LastName
+        {
+
+            get
+            {
+                return lastName;
+            }
+
+        }
+
+
+    }
+
+}
